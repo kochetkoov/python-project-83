c7d60cc3ea82f2008ff40fb1ebac810d58fd4b72
Алексей <kochetkoov.ao@mail.ru>
2024-12-08 13:47:36 +0400

    Stage 1

---
+++ Makefile
@@ -0,0 +1,11 @@
+install:
+	poetry install
+
+lint:
+	poetry run flake8
+
+dev:
+	poetry run flask --app page_analyzer:app run
+
+start:
+	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
---
+++ page_analyzer/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # Загружаем переменные окружения
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key') # Устанавливаем SECRET KEY
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ = '__main__':
+    app.run(debug=True)

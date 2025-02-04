3c294669cb82c6a20b68975518c6136374cb41e0
Алексей <kochetkoov.ao@mail.ru>
2025-02-04 21:14:39 +0400

    update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # скачиваем uv и запускаем команду установки зависимостей
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source /home/kochetkoov/Python/PythonProjects/.venv/bin/activate
+source $HOME/.local/bin/env
 make install && psql -a -d $DATABASE_URL -f database.sql
